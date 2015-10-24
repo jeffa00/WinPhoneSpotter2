8f7a6c684679aa68251c0ac28d44dc3e5ce3755b
jeffa00 <jeffa00@gmail.com>
2015-10-24 10:40:55 -0400

    Initial commit

---
+++ src/PhoneSpotter/Controllers/HomeController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Mvc;
+using PhoneSpotter.Models;
+using Microsoft.Framework.OptionsModel;
+
+namespace PhoneSpotter.Controllers
+{
+    public class HomeController : Controller
+    {
+        private ApplicationOptions Options { get; set; }
+
+        public HomeController(IOptions<ApplicationOptions> options)
+        {
+            Options = options.Value;
+        }
+
+        private void LoadViewData()
+        {
+            ViewData["SiteTitle"] = Options.SiteTitle ?? "Site Title Missing, Shoot Developer";
+        }
+
+        public IActionResult Index()
+        {
+            LoadViewData();
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            LoadViewData();
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            LoadViewData();
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            LoadViewData();
+            return View("~/Views/Shared/Error.cshtml");
+        }
+    }
+}
---
+++ src/PhoneSpotter/Models/ApplicationOptions.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PhoneSpotter.Models
+{
+    public class ApplicationOptions
+    {
+        public string SiteTitle { get; set; }
+    }
+}
