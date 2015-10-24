619e9eec0206bef714f90679f0f6bea5ce4b2a17
jeffa00 <jeffa00@gmail.com>
2015-10-24 11:14:06 -0400

    Added model and viewmodel

---
+++ src/PhoneSpotter/Models/PhoneSpotting.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PhoneSpotter.Models
+{
+    public class PhoneSpotting
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity]
+        public int PhoneSpottingId { get; set; }
+
+        public DateTime SpotTime { get; set; }
+        public string PhoneManufacturer { get; set; }
+        public string PhoneModel { get; set; }
+        public double Latitude { get; set; }
+        public double Longitude { get; set; }
+        public string Notes { get; set; }
+
+    }
+}
---
+++ src/PhoneSpotter/ViewModels/PhoneSpottingsViewModel.cs
@@ -0,0 +1,13 @@
+﻿using PhoneSpotter.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PhoneSpotter.ViewModels
+{
+    public class PhoneSpottingsViewModel
+    {
+        public List<PhoneSpotting> PhoneSpottings { get; set; }
+    }
+}
